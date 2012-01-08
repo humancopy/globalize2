ed69b17a245f9734cddf08cee2046ad04fb80484
humancopy <info@humancopy.net>
2012-01-08 18:59:26 +0100

    Add ability to populate translations table with existing data for default locale.

--- lib/globalize/active_record/migration.rb
+++ lib/globalize/active_record/migration.rb
@@ -40,6 +40,35 @@ module Globalize
         self.connection.remove_index(translation_table_name, :name => translation_index_name) rescue nil
         self.connection.drop_table(translation_table_name)
       end
+
+      # seeding of existing data into translations
+      def move_data_to_translation_table
+        klass = self.class_name.constantize
+        return unless klass.count > 0
+        translated_attribute_columns = klass.first.translated_attributes.keys
+        klass.all.each do |p|
+          attribs = {}
+          translated_attribute_columns.each { |c| attribs[c] = p.read_attribute(c) }
+          p.update_attributes(attribs)
+        end
+      end
+
+      def move_data_to_model_table
+        # Find all of the translated attributes for all records in the model.
+        klass = self.class_name.constantize
+        return unless klass.count > 0
+        all_translated_attributes = klass.all.collect{|m| m.attributes}
+        all_translated_attributes.each do |translated_record|
+          # Create a hash containing the translated column names and their values.
+          translated_attribute_names.inject(fields_to_update={}) do |f, name|
+            f.update({name.to_sym => translated_record[name.to_s]})
+          end
+
+          # Now, update the actual model's record with the hash.
+          klass.update_all(fields_to_update, {:id => translated_record['id']})
+        end
+      end
+      
     end
   end
 end
